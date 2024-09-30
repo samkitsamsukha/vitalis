13f7c80793456475afb34dcbcd5331f529e8f1c4
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-09-30 17:33:27 +0530

    nav start

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div className="flex justify-between p-2  bg-white shadow-md fixed top-0 w-full">
+			<div className="left">
+				<div className="logo"></div>
+				<div className="text-3xl font-bold text-vtgreen">Vitalis</div>
+			</div>
+            <div className="right">
+
+            </div>
+		</div>
+	);
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				blue1: "#5C9EAD", // Coral Blue 1
+				blue2: "#62B6CB", // Coral Blue 2
+				blue3: "#4A8FA1", // Coral Blue 3
+				green1: "#50C878", // Emerald Green 1
+				green2: "#3EB489", // Emerald Green 2
+				green3: "#4BAF89", // Emerald Green 3
+				vtgreen: "#20968b", // vtgreen is defined here
+			},
+		},
+	},
+	plugins: [],
 };
